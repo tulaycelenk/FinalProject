110bb3aeef00f3184a15c58efd4f008996f69d4e
tulaycelenk <tulaycelnk0@gmail.com>
2022-08-08 17:00:20 +0300

    08.08

---
+++ DataAccess/Abstract/IEntityRepository1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract
+{
+
+
+    public interface IEntityRepository<T>
+    {
+        //filter==null filtre vermezse tüm datayı istiyordur
+        //Expression filterleme yapmamızı sağlıyor linqle olduğu gibi
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        //filtre verdi 1 şey gösterecek içerik detay sayfasında olduğu gibi
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context :Db tabloları ile proje classlarını bağlamak
+
+    public class NorthwindContex:DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=");
+        }
+
+    }
+}
